4780e0a502e5a56e8a76096851ed021819ea3ee4
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2023-11-12 21:20:32 +0530

    "feat:variables & const"

---
+++ brad_/javascript.js
@@ -0,0 +1,28 @@
+console.log("hello")
+console.error(" erro");
+console.table({name:"sam",email:"sam@gmail.com"});
+console.warn("warning");
+console.group()
+console.log(100);
+console.log("hello");
+console.error(" erro");
+console.groupEnd;
+
+// variable :let , const ,var( global variable)
+let firstName = "Maurice";
+const lastName = "Dsouza";
+console.log(firstName,lastName);//underdefined//
+const age = 30;
+console.log(age)
+
+const ar = [1,2,3,4];
+// to manipulate array we use push func
+ar.push(1);
+console.log(ar)
+
+// mutiple assign value at once
+let a=27
+b=20
+c=10;
+d=23
+console.log(a,b,c,d)
