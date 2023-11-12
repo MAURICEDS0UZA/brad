888ce57460c1a3515b223f315bdccaaa5fb999cd
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2023-11-12 16:33:27 +0530

    new :first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+console.log("hello")
+console.error(" erro");
+console.table({name:"sam",email:"sam@gmail.com"});
+console.warn("warning");
+console.group()
+console.log(100);
+console.log("hello");
+console.error(" erro");
+console.groupEnd;
