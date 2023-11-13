686f69c3b881ad51cfa89895fc56873ff1991062
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2023-11-13 20:00:09 +0530

    feat:type of conversion

--- brad_/javascript.js
+++ brad_/javascript.js
@@ -1,39 +1,52 @@
-console.log("hello")
-console.error(" erro");
-console.table({sname:"sam",email:"sam@gmail.com"});
-console.warn("warning");
-console.group()
-console.log(100);
-console.log("hello");
-console.error(" erro");
-console.groupEnd;
+// console.log("hello")
+// console.error(" erro");
+// console.table({sname:"sam",email:"sam@gmail.com"});
+// console.warn("warning");
+// console.group()
+// console.log(100);
+// console.log("hello");
+// console.error(" erro");
+// console.groupEnd;
 
-// variable :let , const ,var( global variable)
-let lastsName = "Maurice";
-const firstsName = "Dsouza";
-console.log(firstsName,lastsName);//underdefined//
-const age ="23";
-console.log(age)
+// // variable :let , const ,var( global variable)
+// let lastsName = "Maurice";
+// const firstsName = "Dsouza";
+// console.log(firstsName,lastsName);//underdefined//
+// const age ="23";
+// console.log(age)
 
-const ar = [1,2,3,4];
-// to manipulate array we use push func
-ar.push(1);
-console.log(ar)
+// const ar = [1,2,3,4];
+// // to manipulate array we use push func
+// ar.push(1);
+// console.log(ar)
 
-// mutiple assign value at once
-let a=27
-b=20
-c=10;
-d=23
-console.log(a,b,c,d)
+// // mutiple assign value at once
+// let a=27
+// b=20
+// c=10;
+// d=23
+// console.log(a,b,c,d)
 
-// data type
-const   firstValue = "gesture";
-        lastValue = 100;
-console.log(lastValue,typeof lastValue);
-console.log(firstValue,typeof firstValue);
+// // data type
+// const   firstValue = "gesture";
+//         lastValue = 100;
+// console.log(lastValue,typeof lastValue);
+// console.log(firstValue,typeof firstValue);
 
-const sname='maam';
-let  sName=sname;
+// const sname='maam';
+// let  sName=sname;
 
-console.log(sName,sname);
+// console.log(sName,sname);
+let value="12";
+console.log(value,typeof value);
+// type conversion into number
+
+// value=parseInt(value);
+// value=+(value);
+value=Number(value);
+console.log(value,typeof value);
+
+// convert to string
+let value1 = 12
+output1=value1.toString();
+console.log(output1,typeof output1)
