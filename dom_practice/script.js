eadd1c242fbab903fb90d57f90ccb5ea34b2d4db
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2023-11-27 15:05:30 +0530

    new:file contains js

---
+++ dom_practice/script.js
@@ -0,0 +1,9 @@
+// console.log(document.all)
+// console.log(document.head.children[4])
+// console.log(document.body.children[0])
+
+// console.log(document.URL)
+// console.log(document.domain)
+// console.log(document.links)
+// const output = document.links[0].id = 'google-links'
+console.log(document.links[0].id = 'google-links')
