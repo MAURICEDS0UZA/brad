852538891973640be34c644ace43d64a13c0a61d
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2023-12-14 15:57:14 +0530

    Feat:set & clear interval.

--- asyschronous/script.js
+++ asyschronous/script.js
@@ -1,12 +1,46 @@
-const timerId = setTimeout(changeText, 3000);
-console.log(timerId)
-function changeText() {
-    document.querySelector('h1').style.color = 'red';
-    document.querySelector('h1').style.border = "solid black 1px"
+// const timerId = setTimeout(changeText, 3000);
+// console.log(timerId)
+// function changeText() {
+//     document.querySelector('h1').style.color = 'red';
+//     document.querySelector('h1').style.border = "solid black 1px"
 
-};
+// };
 
-document.querySelector('#cancel').addEventListener('click', () => {
-    console.log('clicked cancel')
-    clearTimeout(timerId);
-})
+// document.querySelector('#cancel').addEventListener('click', () => {
+//     console.log('clicked cancel')
+//     clearTimeout(timerId);
+// })
+
+let intervalId;
+
+function startInterval() {
+    if (!intervalId) {
+        intervalId = setInterval(changeBgColorRandom, 3000);
+    }
+}
+
+// function changeBgColor() {
+//     if (document.body.style.backgroundColor !== 'black') {
+//         document.body.style.color = 'white';
+//         document.body.style.backgroundColor = 'black';
+//     }
+//     else {
+//         document.body.style.color = 'black';
+//         document.body.style.backgroundColor = 'white';
+
+//     }
+// }
+
+// get random color
+function changeBgColorRandom() {
+    const randomIdex = Math.floor(Math.random() * 162323).toString(16);
+    console.log(randomIdex)
+    document.body.style.backgroundColor = `#${randomIdex}`;
+}
+
+function stopInterval() {
+    clearInterval(intervalId)
+}
+
+document.querySelector('#start').addEventListener('click', startInterval);
+document.querySelector('#stop').addEventListener('click', stopInterval);
