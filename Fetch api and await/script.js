2dd39314b471e8a2e7faa32c3f95c47f8d8ae3bb
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-01-18 14:59:02 +0530

    Feat:Post fetch request

--- Fetch api and await/script.js
+++ Fetch api and await/script.js
@@ -1,2 +1,18 @@
-// fetch("https://api.github.com/users/bradtraversy/repos").then((data) => data.json()).then(res => console.log(res)).catch(err => console.log(err));
-fetch("https://api.github.com/users/bradtraversy/repos").then((data) => data.json()).then(res => (document.querySelector("h1").textContent = res[1].name)).catch(err => console.log(err));
+// // fetch("https://api.github.com/users/bradtraversy/repos").then((data) => data.json()).then(res => console.log(res)).catch(err => console.log(err));
+// fetch("https://api.github.com/users/bradtraversy/repos").then((data) => data.json()).then(res => (document.querySelector("h1").textContent = res[1].name)).catch(err => console.log(err));
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'post',
+    body: JSON.stringify({
+        title: 'foo',
+        body: 'basddr',
+        userId: 1,
+    }),
+    headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+    },
+})
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(error => console.error('Error:', error));
+
+
