1539e96201777231eed2db9dbe608abc41f0fda9
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-01-18 15:17:34 +0530

    modified:destructing the object

--- Fetch api and await/script.js
+++ Fetch api and await/script.js
@@ -1,18 +1,21 @@
 // // fetch("https://api.github.com/users/bradtraversy/repos").then((data) => data.json()).then(res => console.log(res)).catch(err => console.log(err));
 // fetch("https://api.github.com/users/bradtraversy/repos").then((data) => data.json()).then(res => (document.querySelector("h1").textContent = res[1].name)).catch(err => console.log(err));
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'post',
-    body: JSON.stringify({
-        title: 'foo',
-        body: 'basddr',
-        userId: 1,
-    }),
-    headers: {
-        'Content-Type': 'application/json; charset=UTF-8'
-    },
-})
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(error => console.error('Error:', error));
+function createPost({ title, body }) {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'post',
+        body: JSON.stringify({
+            title,
+            body,
+            userId: 1,
+        }),
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            token: '12ns'
+        },
+    })
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(error => console.error('Error:', error));
+}
 
-
+createPost({ title: 'hwlllo', body: "this is me" })
