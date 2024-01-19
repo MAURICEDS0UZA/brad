c0875b152f655bfd05477aec8babc8cd03429bac
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-01-19 10:42:51 +0530

    feat: Add new item using fetch(post method).

--- Typecode_Todos_mini_project/script.js
+++ Typecode_Todos_mini_project/script.js
@@ -1,6 +1,6 @@
-const url = 'https://jsonplaceholder.typicode.com/todos' + `?_limit=5`
+const url = 'https://jsonplaceholder.typicode.com/todos';
 
-fetch(url)
+fetch(url + `?_limit=5`)
     .then(res => res.json())
     .then(data => {
         getData(data)
@@ -9,17 +9,42 @@ fetch(url)
 
 // getting title and status
 function getData(data) {
-    data.forEach(ele => {
-        const div = document.createElement("div");
-        div.textContent = `${ele.title}`;
-        div.style.border = "1px solid black";
-        div.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-        document.querySelector("#todo-list").append(div);
+    data.forEach(ele => { addToDom(ele) });
+}
 
-    });
+function addToDom(ele) {
+    const div = document.createElement("div");
+    div.style.border = "1px solid black";
+    div.textContent = `${ele.title}`;
+    console.log(ele.completed)
+    // change color for complted status
+    if (ele.completed) {
+        div.style.backgroundColor = `#2fcc16`;
+    } else {
+        div.style.backgroundColor = `#edc566`;
+    }
+    // add to Dom
+    document.querySelector("#todo-list").append(div);
 }
 
 
+const createTodo = (e) => {
+    e.preventDefault();
 
-const btnSubmit = document.querySelector('button[type="submit"]');
-btnSubmit.addEventListener(onclick, (e) => { console.log('e') });
+    const newTodo = {
+        title: e.target.firstElementChild.value,
+        completed: false,
+    };
+
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(newTodo),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => addToDom(data));
+};
+
+document.querySelector('#todo-form').addEventListener('submit', createTodo);
